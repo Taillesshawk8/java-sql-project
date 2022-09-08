699858deccbc5fdb4fef0938ecd102875b266356
Nicholas Vanderlinden <watchout8181@gmail.com>
2022-09-08 10:06:22 +0200

    almost done with step 5

--- src/test/java/sql/SqlTest.java
+++ src/test/java/sql/SqlTest.java
@@ -1,21 +1,40 @@
 package sql;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.sql.*;
 
 public class SqlTest {
 
-    // Retrieve the number of customers from the database
-    // Check that the total number of customers is equal to 2
     @Test
-    public void getAllCustomers_checkNumber_shouldBeTwo() {
-
+    public void getAllCustomers_checkNumber_shouldBeTwo() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sqlproject", "root", "Gl@d0s");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(
+                "select * " +
+                "from customers");
+        int rsCount = 0;
+        while(resultSet.next()) {
+            rsCount = rsCount + 1;
+        }
+        Assertions.assertEquals(2,rsCount);
+        System.out.println("Number of customers: "+rsCount);
     }
 
-    // Retrieve all accounts for customer Sarah from the database
-    // Check that the total number of accounts for her is 2
     @Test
-    public void getAccountsForSarah_checkNumber_shouldBeTwo() {
-
+    public void getAccountsForSarah_checkNumber_shouldBeTwo() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sqlproject", "root", "Gl@d0s");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(
+                "SELECT * FROM account\n" +
+                "where CustomerID=1;");
+        int rsCount = 0;
+        while(resultSet.next()) {
+            rsCount = rsCount + 1;
+        }
+        Assertions.assertEquals(2,rsCount);
+        System.out.println("Number of account for Mikasa: "+rsCount);
     }
 
     // A test that checks that the total of all transactions is equal to 0 for a given account.
